ffbdeeb71115cd29741ef5fdfe1e82fa020c0e33
Tinjus99 <156869630+Tinjus99@users.noreply.github.com>
2025-05-01 13:20:59 -0600

    work on functions C# prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -6,4 +6,22 @@ class Program
     {
         Console.WriteLine("Hello Prep5 World!");
     }
+    static void DisplayWelcome(string[] args)
+    {
+        Console.WriteLine("Welcome to the C# Prep5 program.");
+    }
+    static void PromptUserName(string[] args)
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+    }
+    static void PromptUserNumber(int[] args)
+    {
+        Console.Write("What is your favorite number? ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+    }
+    static void SquareNumber(int favoriteNumber)
+    {
+        int square = favoriteNumber * favoriteNumber;
+    }
 }
