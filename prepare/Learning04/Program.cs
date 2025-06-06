19b0b315ebaaa1cc014bfc489aa245a86d6ecb39
Tinjus99 <156869630+Tinjus99@users.noreply.github.com>
2025-06-06 00:08:49 -0600

    learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+
+        Assignment a1 = new Assignment("Sam", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Math m1 = new Math("Robert", "Fractions", "7.3", "8-19");
+        Console.WriteLine(m1.GetHomeworkList());
+
+
+        Writing w1 = new Writing("Mary", "European History", "The Causes of World War II");
+        Console.WriteLine(w1.GetWritingInformation());
     }
 }
